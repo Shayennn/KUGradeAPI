e3dfcdc909bf2a0e04730778a4e4b162f9f3b1e1
Shayennn <me@shayennn.com>
2020-06-07 18:46:58 +0700

    Added Semester choose to API

--- index.php
+++ index.php
@@ -1,19 +1,25 @@
 <?php
 require_once('KUGrade.php');
 
-function post_main(){
+function post_main()
+{
     header("Access-Control-Allow-Origin: *");
     header('Content-Type: application/json');
     $obj = new KUGrade($_POST['username'], $_POST['password']);
-    echo json_encode($obj->getGrade());
+    if (array_key_exists('semester', $_POST)) {;
+        echo json_encode($obj->getGrade($_POST['semester']));
+    } else {
+        echo json_encode($obj->getGrade());
+    }
 }
 
-function get_main(){
+function get_main()
+{
     header('Content-Type: text/plain');
     echo file_get_contents('help.txt');
 }
 
-if($_SERVER['REQUEST_METHOD'] === 'POST')
+if ($_SERVER['REQUEST_METHOD'] === 'POST')
     post_main();
 else
     get_main();
